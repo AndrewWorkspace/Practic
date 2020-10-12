9cc5cfda01d6a0ffbfd235ba9ff31515b975945e
Admin <mirror.workspace@gmail.com>
2020-10-12 20:06:45 +0300

    Bug Fix

--- 1) Spread Operator/js/script.js
+++ 1) Spread Operator/js/script.js
@@ -1,20 +1,25 @@
 const a = {
-    b: {
-        c: 25,
-        m: {
-            k: 'asd',
-            p: 10
-        }
-    },
-    t: 'm',
-    y: {
-        v: 5
-    }
+	b: {
+		c: 25,
+		m: {
+			k: 'asd',
+			p: 10
+		}
+	},
+	t: 'm',
+	y: {
+		v: 5
+	}
 };
 
-const b = a;
-b.b.m.now = 2020;
-console.log(b);
+const b = {
+	...a,
+	b: {
+		...a.b,
+		now: 2020
+	}
+}
 
-const {y:{v: getV}} = b
+
+const {y: {v: getV}} = b
 console.log(getV)
